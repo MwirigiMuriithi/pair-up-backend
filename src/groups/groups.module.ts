6c644ca117b4f7a1ad1efb8895860cda9369e0bb
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 23:33:08 +0300

    fix(groups): import TypeOrmModule.forFeature([Group]) in GroupsModule

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { GroupsController } from './groups.controller';
-import { GroupsService } from './groups.service';
+// src/groups/groups.module.ts
+import { Module }             from '@nestjs/common';
+import { TypeOrmModule }      from '@nestjs/typeorm';
+
+import { GroupsService }      from './groups.service';
+import { GroupsController }   from './groups.controller';
+import { Group }              from './entities/group.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Group]),
+  ],
   controllers: [GroupsController],
-  providers: [GroupsService]
+  providers: [GroupsService],
+  exports: [GroupsService],
 })
 export class GroupsModule {}
+
