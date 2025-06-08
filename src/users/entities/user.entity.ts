4e6ec3bc1208263e20ec9ba7de3dc4dc4fa7190f
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 22:55:52 +0300

    Add User entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid') id: string;
+
+  @Column()      username: string;
+  @Column({unique: true}) email: string;
+  @Column()      passwordHash: string;
+}
