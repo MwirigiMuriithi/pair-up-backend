9df37ce878a7b5d8614a9ffc5b20e1c84e07dfbc
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 22:48:40 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
+import { TodayModule } from './today/today.module';
+import { EventsModule } from './events/events.module';
+import { BucketModule } from './bucket/bucket.module';
+import { HeartsModule } from './hearts/hearts.module';
+import { DreamsModule } from './dreams/dreams.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, GroupsModule, TodayModule, EventsModule, BucketModule, HeartsModule, DreamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
