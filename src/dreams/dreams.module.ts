ab80564fb8b56efc2ffc8dce026cc0317f84765f
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-10 11:56:08 +0300

    Update dreams.module.ts

--- src/dreams/dreams.module.ts
+++ src/dreams/dreams.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { DreamsController } from './dreams.controller';
-import { DreamsService } from './dreams.service';
+import { Module }          from '@nestjs/common';
+import { TypeOrmModule }   from '@nestjs/typeorm';
+import { Dream }           from './entities/dream.entity';
+import { DreamsService }   from './dreams.service';
+import { DreamsController} from './dreams.controller';
 
 @Module({
-  controllers: [DreamsController],
-  providers: [DreamsService]
+  imports: [TypeOrmModule.forFeature([Dream])],
+  providers: [DreamsService],
+  controllers:[DreamsController],
 })
 export class DreamsModule {}
