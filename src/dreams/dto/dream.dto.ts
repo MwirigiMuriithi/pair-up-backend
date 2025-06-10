19f20da7c5a3e732cf737ae3a6cab3d36877519a
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-10 12:01:59 +0300

    Add: dream.dto.ts

---
+++ src/dreams/dto/dream.dto.ts
@@ -0,0 +1,10 @@
+export class DreamDto {
+  id: string;
+  groupId: string;
+  createdBy: string;
+  updatedBy: string;
+  title: string;
+  description: string;
+  isAchieved: boolean;
+  targetDateMillis: number;
+}
