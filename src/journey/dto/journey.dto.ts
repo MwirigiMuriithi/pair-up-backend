83e8d1f63ae9107a56e0bf03c5c5a38d1b4646a6
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-10 12:02:00 +0300

    Add: journey.dto.ts

---
+++ src/journey/dto/journey.dto.ts
@@ -0,0 +1,10 @@
+export class JourneyDto {
+  id: string;
+  groupId: string;
+  createdBy: string;
+  updatedBy: string;
+  title: string;
+  description: string;
+  journeyDateMillis: number;
+  type: string;
+}
